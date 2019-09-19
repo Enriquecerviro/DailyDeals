7029c21592e98f72400a41bf4e32d106d3ae51fa
Enrique Cervino <enrique.cervirodam@gmail.com>
2019-09-19 09:44:42 +0200

    firstServerCommit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+'use strict'
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require ('body-parser');
+
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
